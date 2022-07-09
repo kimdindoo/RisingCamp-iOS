cc5c07c5558f511834e0a46b45f860f2beb02173
Jinsoo <aaa119o1125@gmail.com>
2022-07-10 03:20:32 +0900

    NewsViewController tableVieww add

---
+++ RisingCamp 5week/RisingCamp 5week/EatDealData.swift
@@ -0,0 +1,8 @@
+//
+//  EatDealData.swift
+//  RisingCamp 5week
+//
+//  Created by 김진수 on 2022/07/10.
+//
+
+import Foundation
---
+++ RisingCamp 5week/RisingCamp 5week/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  RisingCamp 5week
+//
+//  Created by 김진수 on 2022/07/10.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
